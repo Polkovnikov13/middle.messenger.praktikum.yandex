3aa60cdbaef26cbb386b00409e5f1afb1aa3da3a
Artem Polkovnikov <artem.polkovnikov93@yandex.ru>
2023-10-01 20:58:49 +0300

    end

--- src/pages/User/index.ts
+++ src/pages/User/index.ts
@@ -6,7 +6,7 @@ import { Avatar } from '../../components/Avatar';
 
 export const User = () => {
   return Handlebars.compile(tmpl)({
-    avaAvatar: Avatar({imageName: '../../images/avatars/photo_2023.jpg', imageText: 'no photo',imageClass: 'user-avatar'}),
+    avaAvatar: Avatar({imageName: 'https://i.pinimg.com/originals/01/4e/f2/014ef2f860e8e56b27d4a3267e0a193a.jpg', imageText: 'no photo',imageClass: 'user-avatar'}),
     editUserMainLink: Link({ to: '/user-edit-data', text: 'Изменить данные'}),
     editUserPasswordLink: Link({ to: '/user-edit-password', text: 'Изменить пароль'}),
     exitLink: Link({ to: '/', text: 'Выйти'}),
