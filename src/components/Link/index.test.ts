9bcdd13ef4cfa267148a44e985a7c5e3dd907a48
Artem Polkovnikov <artem.polkovnikov93@yandex.ru>
2023-11-04 02:30:34 +0300

    done

---
+++ src/components/Link/index.test.ts
@@ -0,0 +1,58 @@
+import { Link } from './index.ts';
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+describe('Link', () => {
+  it('should render', () => {
+    new Link({ to: '/' });
+  });
+
+  it('element should return div', () => {
+    const link = new Link({ to: '/' });
+    const element = link.element;
+    expect(element).to.be.instanceof(window.HTMLDivElement)
+  });
+
+  it('should set href correctly', () => {
+  const to = '/path';
+  const link = new Link({ to });
+  const element = link.element as HTMLDivElement;
+  const anchorElement = element.querySelector('a');
+  expect(anchorElement).to.not.be.null;
+  if (anchorElement) {
+    expect(anchorElement.getAttribute('href')).to.equal(to);
+  }
+});
+  it('should be clickable', () => {
+    const callback = sinon.stub();
+    const props = {
+      to: '/',
+      text: 'Кнопка',
+      events: {
+        click: callback,
+      },
+    };
+    const link = new Link(props);
+    const element = link.element as HTMLDivElement;
+    element.click();
+    expect(callback.calledOnce).to.eq(true);
+  });
+  it('should not set event handlers when events are not provided', () => {
+  const link = new Link({ to: '/' });
+  const element = link.element as HTMLDivElement;
+  expect(element.onclick).to.be.null;
+});
+  it('should render text correctly', () => {
+  const text = 'Ссылка';
+  const link = new Link({ to: '/', text });
+  const element = link.element as HTMLDivElement;
+  expect(element.textContent).to.equal(text);
+});
+  it('should not render text when text is not provided', () => {
+  const link = new Link({ to: '/' });
+  const element = link.element as HTMLDivElement;
+  expect(element.textContent).to.equal('');
+});
+
+});
+
---
+++ src/core/HTTPTransport/index.test.ts
@@ -0,0 +1,71 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { expect } from 'chai';
+import sinon, { SinonFakeXMLHttpRequest, SinonFakeXMLHttpRequestStatic} from 'sinon';
+import { HTTPTransport, Method } from './index.ts';
+import { addListener } from 'process';
+
+describe('HTTPTransport', () => {
+  let xhr: SinonFakeXMLHttpRequestStatic;
+  const instance = new HTTPTransport('');
+  const requests: SinonFakeXMLHttpRequest[] = [];
+  beforeEach(() => {
+    xhr = sinon.useFakeXMLHttpRequest();
+
+    //@ts-expect-error
+    global.XMLHttpRequest = xhr;
+
+    xhr.onCreate = (req) => {
+      requests.push(req);
+    };
+  });
+
+  afterEach(() => {
+    requests.length = 0;
+    xhr.restore();
+  });
+
+  describe('Method', () => {
+    it('get() should be called with GET method', () => {
+      instance.get('/');
+      expect(requests[0].method).to.eq(Method.Get);
+    });
+    it('post() should be called with POST method', () => {
+      instance.post('/');
+      expect(requests[0].method).to.eq(Method.Post);
+    });
+    it('put() should be called with PUT method', () => {
+      instance.put('/',{});
+      expect(requests[0].method).to.eq(Method.Put);
+    });
+    it('delete() should be called with DELETE method', () => {
+      instance.delete('/');
+      expect(requests[0].method).to.eq(Method.Delete);
+    });
+  });
+
+    it('post() should send data in the request body', () => {
+      const data = { key: 'value' };
+      instance.post('/', data);
+      expect(requests[0].requestBody).to.eq(JSON.stringify(data));
+});
+    it('put() should send data in the request body', () => {
+      const data = { key: 'value' };
+      instance.put('/', data);
+      expect(requests[0].requestBody).to.eq(JSON.stringify(data));
+});
+    it('patch() should send data in the request body', () => {
+      const data = { key: 'value' };
+      instance.patch('/', data);
+      expect(requests[0].requestBody).to.eq(JSON.stringify(data));
+});
+    it('should set Content-Type header for POST requests', () => {
+      const formData = new FormData();
+      instance.post('/', formData);
+      expect(requests[0].requestHeaders['Content-Type']).to.be.undefined;
+});
+    it('should send data as FormData for specific requests', () => {
+      const formData = new FormData();
+      instance.post('/', formData);
+      expect(requests[0].requestBody).to.eq(formData); 
+});
+}); 
