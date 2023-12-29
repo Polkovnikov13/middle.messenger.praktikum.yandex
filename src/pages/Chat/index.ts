bafe43e03a0528b95f99322d4cd883affcad4312
Artem Polkovnikov <artem.polkovnikov93@yandex.ru>
2023-12-29 12:41:37 +0300

    dep1.6

--- src/pages/Chat/index.ts
+++ src/pages/Chat/index.ts
@@ -309,7 +309,7 @@ const mapStateToProps = (state: IState) => ({
   wsMessage: state.wsMessage ? state.wsMessage.map((message: WSMessage) => {
     const foundUser = state.xFiles?.find((user:any) => user.id === message.user_id);
     const name = foundUser ? foundUser.login : "Фиксик";
-    const isSentMessage = message.user_id === state.user.id; // Проверка, отправлено ли сообщение текущим пользователем
+    const isSentMessage = message.user_id === state.user?.id; // Используйте опциональный оператор "?"
     return { ...message, name, isSentMessage };
   }) : [],
   mesId: state.mesId,
