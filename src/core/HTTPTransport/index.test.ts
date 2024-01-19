2d9f7e1c2b50bd26e174a26a67c49bf1501a1245
Artem Polkovnikov <artem.polkovnikov93@yandex.ru>
2024-01-19 12:15:30 +0300

    audit

--- src/core/HTTPTransport/index.test.ts
+++ src/core/HTTPTransport/index.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { expect } from 'chai';
 import Sinon, { SinonFakeXMLHttpRequest, SinonFakeXMLHttpRequestStatic} from 'sinon';
 import { HTTPTransport, Method } from './index.ts';
